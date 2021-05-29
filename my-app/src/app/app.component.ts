5d1086b353c6c40352a2b2d6c79df5086ed3251a
raman-ppp <ramandeep@hashchainft.com>
2021-05-29 15:41:52 +0530

    Added chart

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -6,5 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  dataSource: Object;
+  title: string;
+
+  constructor() {
+    this.title = 'fc-demo';
+
+    this.dataSource =  {
+      chart: {
+        //Set the chart caption
+        caption: "Countries With Most Oil Reserves [2017-18]",
+        //Set the chart subcaption
+        subCaption: "In MMbbl = One Million barrels",
+        //Set the x-axis name
+        xAxisName: "Country",
+        //Set the y-axis name
+        yAxisName: "Reserves (MMbbl)",
+        numberSuffix: "K",
+        //Set the theme for your chart
+        theme: "fusion"
+      },
+      // Chart Data - from step 2
+      data: [
+        {
+          label: "Venezuela",
+          value: "290"
+        },
+        {
+          label: "Saudi",
+          value: "260"
+        },
+        {
+          label: "Canada",
+          value: "180"
+        },
+        {
+          label: "Iran",
+          value: "140"
+        },
+        {
+          label: "Russia",
+          value: "115"
+        },
+        {
+          label: "UAE",
+          value: "100"
+        },
+        {
+          label: "US",
+          value: "30"
+        },
+        {
+          label: "China",
+          value: "30"
+        }
+      ]
+    };
+  }
 }
